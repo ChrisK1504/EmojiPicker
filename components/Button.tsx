40fb14f8167118cd1e9d3c91b054d42614ec5086
ChrisK1504 <krismehilli@yahoo.com>
2024-10-16 19:23:07 +0200

    Applied different styling to the Buttons by utilizing inline styling, and its property to override styleSheet styling;

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,14 +1,33 @@
 import { Pressable, View, Text, StyleSheet } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
 
 type Props = {
   label: string;
-}
+  theme?: 'primary';
+};
 
-export default function Button({ label }: Props) {
+export default function Button({ label, theme }: Props) {
+  if (theme === 'primary') {
+    return (
+      <View
+        style={[
+          styles.buttonContainer,
+          { borderWidth: 4, borderColor: '#ffd33d', borderRadius: 18 },
+        ]}>
+        <Pressable
+          style={[styles.button, { backgroundColor: '#fff' }]}
+          onPress={() => alert('You Presssed a button')}>
+          <FontAwesome name="picture-o" size={18} color="#25292e" style={styles.buttonIcon} />
+          <Text style={[styles.buttonLabel, { color: '#25292e' }]}>{label}</Text>
+        </Pressable>
+      </View>
+    );
+  }
+
   return (
-    <View>
-      <Pressable onPress={() => alert('You pressed a button.')}>
-        <Text>{label}</Text>
+    <View style={styles.buttonContainer}>
+      <Pressable style={styles.button} onPress={() => alert('You pressed a button')}>
+        <Text style={styles.buttonLabel}>{label}</Text>
       </Pressable>
     </View>
   );
@@ -34,5 +53,8 @@ const styles = StyleSheet.create({
   buttonLabel: {
     color: '#fff',
     fontSize: 16,
+  },
+  buttonIcon: {
+    paddingRight: 8,
   },
 });
