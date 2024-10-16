36a7b43cd08282bd0963150db8db6dfe5bf4cbb3
ChrisK1504 <krismehilli@yahoo.com>
2024-10-16 19:33:09 +0200

    Imported an ImagePicker library, and allowed the first button to access its functionality (to pick a photo from the device's gallery);

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,5 @@
 import { Text, View, StyleSheet } from "react-native";
+import * as ImagePicker from 'expo-image-picker';
 
 import Button from "@/components/Button";
 import ImageViewer from "@/components/ImageViewer";
@@ -7,13 +8,25 @@ import ImageViewer from "@/components/ImageViewer";
 const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
+  const pickImageAsynch = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      console.log(result);
+    } else {
+      alert('You did not select any image.')
+    }
+  }
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer imgSource={PlaceholderImage} />
       </View>
       <View style={styles.footerContainer}>
-        <Button theme="primary" label="Choose a photo" />
+        <Button onPress={pickImageAsynch} theme="primary" label="Choose a photo" />
         <Button label="Use this photo" />
       </View>
     </View>
